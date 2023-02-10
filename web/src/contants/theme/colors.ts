5a9044664dbdc1b0bb88d2f40dd3508e824aeed5
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-10 16:26:23 -0300

    feat(theme)

---
+++ web/src/contants/theme/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  primary: '#DBFF73',
+  secondary: '#B8A9FB',
+  tertiary: '#BBE6EC',
+  quaternary: '#6DD4C1',
+  background: '#F7FCFA',
+  heading: '#212025',
+  foreground: '#F2F4F3',
+  text: '#A5A7A9',
+};
