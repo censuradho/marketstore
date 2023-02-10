e6d38594a8dac50513f6a6bbea628d4235f151ef
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-02-10 12:46:24 -0300

    feat(cloudinary): upload image

--- backend/src/features/cloudinary/cloudinary.service.ts
+++ backend/src/features/cloudinary/cloudinary.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
+import sharp from 'sharp'
 
+import { 
+  UploadApiErrorResponse, 
+  UploadApiResponse, 
+  v2 as cloudinary,
+  UploadApiOptions 
+} from 'cloudinary';
+
+import { Readable } from "node:stream";
 @Injectable()
-export class CloudinaryService {}
+export class CloudinaryService {
+  private logger = new Logger(CloudinaryService.name);
+
+  async uploadImage(
+    file: Express.Multer.File,
+    options?: UploadApiOptions,
+    sharpOptions?: sharp.SharpOptions,
+  ): Promise<UploadApiResponse | UploadApiErrorResponse>  {
+    return new Promise(async (resolve, reject) => {
+      const upload = cloudinary
+        .uploader
+        .upload_stream(options, (error, result) => {
+          if (error) {
+            this.logger.error(error);
+            return reject(error);
+          }
+          return resolve(result)
+        })
+        
+        const stream: Readable = new Readable();
+
+        if (sharpOptions && file.mimetype.match(/^image/)) {
+          const shrinkedImage = await sharp(file.buffer).toBuffer();
+  				stream.push(shrinkedImage)
+        } else {
+          stream.push(file.buffer);
+        }
+
+        stream.push(null);
+
+        stream.pipe(upload);
+    })
+  }
+}
